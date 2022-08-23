343e2e73b5a1165ae9e859d09d834d22994cc25b
sz.wong <szezhen.dev@gmail.com>
2022-08-23 17:03:34 +0800

    Added liquibase

--- src/main/java/com/aquariux/cryptotrading/CryptoTradingApplication.java
+++ src/main/java/com/aquariux/cryptotrading/CryptoTradingApplication.java
@@ -13,12 +13,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
 import com.aquariux.cryptotrading.config.AquariuxConstants;
 import com.aquariux.cryptotrading.config.DefaultProfileUtil;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class CryptoTradingApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(CryptoTradingApplication.class);
--- src/main/java/com/aquariux/cryptotrading/config/AquariuxConstants.java
+++ src/main/java/com/aquariux/cryptotrading/config/AquariuxConstants.java
@@ -28,5 +28,8 @@ public interface AquariuxConstants {
     String SPRING_PROFILE_DEVELOPMENT = "dev";
     /** Constant <code>SPRING_PROFILE_PRODUCTION="prod"</code> */
     String SPRING_PROFILE_PRODUCTION = "prod";
+    /** Spring profile used to disable running liquibase
+	Constant <code>SPRING_PROFILE_NO_LIQUIBASE="no-liquibase"</code> */
+    String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
 	
 }
